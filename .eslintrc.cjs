fc1948a80a960e4dce54a581e9c19e0ac70637bd
LucasOe <github@lucasoe.com>
2024-02-24 05:36:11 +0100

    Disable ESLint type checking for js files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,4 @@
+/* eslint-env node */
 module.exports = {
 	root: true,
 	env: { browser: true, es2020: true },
@@ -10,17 +11,21 @@ module.exports = {
 		"plugin:tailwindcss/recommended",
 		"prettier",
 	],
-	ignorePatterns: ["dist", ".eslintrc.cjs"],
+	ignorePatterns: ["dist"],
 	settings: { react: { version: "detect" } },
 	parser: "@typescript-eslint/parser",
 	parserOptions: {
-		ecmaVersion: "latest",
-		sourceType: "module",
 		project: ["./tsconfig.json", "./tsconfig.node.json"],
 		tsconfigRootDir: __dirname,
 	},
-	plugins: ["react-refresh"],
+	plugins: ["@typescript-eslint", "react-refresh"],
 	rules: {
 		"react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
 	},
+	overrides: [
+		{
+			files: ["**/*.{js,jsx,cjs}"],
+			extends: ["plugin:@typescript-eslint/disable-type-checked"],
+		},
+	],
 };
